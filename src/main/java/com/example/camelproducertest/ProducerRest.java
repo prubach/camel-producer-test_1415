500ec0514c602f04f838977410ca50884e2fc94c
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-04-29 14:56:07 +0200

    Return student object in JSON

--- src/main/java/com/example/camelproducertest/ProducerRest.java
+++ src/main/java/com/example/camelproducertest/ProducerRest.java
@@ -1,8 +1,12 @@
 package com.example.camelproducertest;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
 public class ProducerRest extends RouteBuilder {
@@ -15,9 +19,24 @@ public class ProducerRest extends RouteBuilder {
 
         rest("/student")
                 .produces("application/json")
-                .get("/hello/{name}")
-                .route().transform().simple("Witam ${header.name} !!!")
-                .endRest();
+                .get("/get/{fname}/{lname}")
+                .to("direct:student");
+                //.route().transform().simple("Witam ${header.name} !!!")
+                //.endRest();
+
+        from("direct:student")
+                .process(new Processor() {
+                    final AtomicLong counter = new AtomicLong();
+
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        String fname = exchange.getIn().getHeader("fname", String.class);
+                        String lname = exchange.getIn().getHeader("lname", String.class);
+                        exchange.getIn().setBody(new Student(counter.incrementAndGet(), fname, lname));
+                    }
+                })
+                .log("${body}");
+
 
     }
 }
